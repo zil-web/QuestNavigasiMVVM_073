253566878f3d3061983f86ef4e8b67ff8ef407a0
zil-web <126854462+zil-web@users.noreply.github.com>
2024-11-21 21:39:10 +0700

    menambahkan penjelasan DataKelamin pada code

--- app/src/main/java/com/example/questnavigasimvvm_073/model/DataKelamin.kt
+++ app/src/main/java/com/example/questnavigasimvvm_073/model/DataKelamin.kt
@@ -1,12 +1,13 @@
 package com.example.questnavigasimvvm_073.model
 
 import com.example.questnavigasimvvm_073.R
-
+//Objek DataKelamin berfungsi untuk menyediakan data jenis kelamin yang digunakan dalam aplikasi.
+//Data ini diambil dari file string resources dan disimpan dalam bentuk list.
 object DataKelamin {
 
 
     val listJk = listOf(
-        R.string.laki,
-        R.string.wanita
+        R.string.laki, // Menyimpan ID resource string untuk "Laki-laki"
+        R.string.wanita // Menyimpan ID resource string untuk "Perempuan"
     )
 }
