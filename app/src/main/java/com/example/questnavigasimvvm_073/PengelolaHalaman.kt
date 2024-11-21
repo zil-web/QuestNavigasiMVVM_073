f9905e673c4628082c6064c453988dcf177ca37b
zil-web <126854462+zil-web@users.noreply.github.com>
2024-11-21 20:40:34 +0700

    Mengintegrasikan navigasi antar halaman dengan Jetpack Compose dan arsitektur MVVM. Commit ini menyediakan kerangka kerja untuk menginput, menyimpan, dan menampilkan data mahasiswa, dengan dukungan navigasi dinamis antar halaman. Tata letak utama juga disiapkan menggunakan `Scaffold` untuk fleksibilitas dalam desain UI.

--- app/src/main/java/com/example/questnavigasimvvm_073/PengelolaHalaman.kt
+++ app/src/main/java/com/example/questnavigasimvvm_073/PengelolaHalaman.kt
@@ -1,29 +1,52 @@
 package com.example.questnavigasimvvm_073
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
+import com.example.questnavigasimvvm_073.ui.theme.view.DataMahasiswaView
+import com.example.questnavigasimvvm_073.ui.theme.view.FormMahasiswaView
 import com.example.questnavigasimvvm_073.ui.theme.viewmodel.MahasiswaViewModel
 
-enum class  Halaman {
-    Formulir,
-    Detail,
-}
 @Composable
-fun PengelolaHalaman(
-    navController: NavController = rememberNavController(),
+fun PengelolaanHalaman(
+    modifier: Modifier = Modifier,
+    navHost: NavHostController = rememberNavController(),
     viewModel: MahasiswaViewModel = viewModel()
 ){
-    val stateUi by view
-    NavHost(navController = navController, startDestination = Halaman.Formulir.name) {
-        composable(route = Halaman.Formulir.name)
-        val konteks = LocalContext.current
-        MainScren(
-            listJK = DataJenisKelamin.listJK.map{id->
-                konteks.resources.getString()
+    Scaffold { isipadding ->
+        val uiState by viewModel.uiState.collectAsState()
+        NavHost(
+            modifier = modifier.padding(isipadding),
+            navController = navHost, startDestination = Halaman.Formulir.name
+        ) {
+            composable(route = Halaman.Formulir.name) {
+                val konteks = LocalContext.current
+                FormMahasiswaView (
+                    listJK = DataKelamin.listJK.map {
+                            isi -> konteks.resources.getString(isi)
+                    },
+                    onSubmitClicked = {
+                        viewModel.saveDataMahasiswa(it)
+                        navHost.navigate(Halaman.Detail.name)
+                    }
+                )
             }
-        )
-    }
+            composable(route = Halaman.Detail.name){
+                DataMahasiswaView(
+                    uiStateMahasiswa = uiState,
+                    onClickButton = {
+                        navHost.popBackStack()
+                    }
+                )
+            }
+            }
+        }
 }
