7b0b1a9d16b5a089268e44e94f66975a9a6dec02
zil-web <126854462+zil-web@users.noreply.github.com>
2024-11-21 19:43:17 +0700

    melanjutkan codingan sebelumnya

---
+++ app/src/main/java/com/example/questnavigasimvvm_073/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.questnavigasimvvm_073
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.questnavigasimvvm_073.ui.theme.QuestNavigasiMVVM_073Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            QuestNavigasiMVVM_073Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    QuestNavigasiMVVM_073Theme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/example/questnavigasimvvm_073/model/DataJK.kt
@@ -0,0 +1,8 @@
+package com.example.questnavigasimvvm_073.model
+
+object DataJK {
+    val isiJK = listOf(
+        R.string.laki,
+        R.string.wanita
+    )
+}
